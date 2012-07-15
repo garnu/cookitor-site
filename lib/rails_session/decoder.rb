2c1a45e51109a4589eebd2c2c046ce197ecbd779
Arnaud Germis <agermis@gmail.com>
2012-07-15 16:39:37 +0100

    Add logging and better validate inputs

--- lib/rails_session/decoder.rb
+++ lib/rails_session/decoder.rb
@@ -2,26 +2,37 @@ module RailsSession
 	class Decoder
 		attr_reader :data, :hash, :error
 		def initialize(content)
-			unless content.blank?
-				clean_content = CGI.unescape(content.split("\n").join)
-				content_parts = clean_content.split('--')
-				if content_parts.size == 2
-					@hash = content_parts.last
-					begin
-						@data = Marshal.load(Base64.decode64(content_parts.first))
-					rescue Exception => e
-						@error = e.to_s
-					end
-				else
-					@error = "Invalid format: couldn't split between the data and the HMAC."
-				end
+			if content.present? && content.size > 42 && content.size <= 4096
+				parse(content) unless content.blank?
 			else
-				@error = "No Rails session to decode."
+				@error = "Invalid cookie."
 			end
 		end
 
 		def valid?
 			error.blank?
+		end
+
+		private
+
+		def parse(content)
+			clean_content = CGI.unescape(content.split("\n").join)
+			content_parts = clean_content.split('--')
+			if content_parts.size == 2
+				@hash = content_parts.last
+				begin
+					decoded = Base64.decode64(content_parts.first).taint.to_s
+					Rails.logger.debug "Decoded from base64: #{decoded.inspect}"
+					@data = Marshal.load(decoded).taint
+					Rails.logger.debug "Loaded object #{@data.inspect}"
+				rescue Exception => e
+					@data = nil
+					@hash = nil
+					@error = e.to_s
+				end
+			else
+				@error = "Invalid format: couldn't split between the data and the HMAC."
+			end
 		end
 	end
 end
