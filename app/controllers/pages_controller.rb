a515575fbf20dcc71e87ae4f054e42840853d0a3
Arnaud Germis <agermis@gmail.com>
2012-07-19 20:08:26 +0100

    Add ping page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
   	session[:hello] = 'world!'
   end
+
+  def ping
+    render text: "Pong!\n#{Time.now}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Cookitor::Application.routes.draw do
   root :to => "pages#home"
 
   post "decode" => "rails_sessions#decode"
+  
+  match "ping"  => "pages#ping"
 end
