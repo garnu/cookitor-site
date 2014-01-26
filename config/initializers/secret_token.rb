7a9cde8e0473d357eefae7219a37fa7694465aaa
Arnaud Germis <agermis@gmail.com>
2014-01-26 19:23:20 +0000

    Set Rails 3 secret token as environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,11 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cookitor::Application.config.secret_token = '69940e024deedb28b5d20071e9a4ca7e7eb23cd09b7ccf6ad2211e92d0c9f5554b50c2920ebd3a01c2c03791928ddfa794fbad1bdda33a5f0b9382476bf7be58'
+
+Cookitor::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN'] || '745b6c90b4ca43d246d3259a486e7adde4db1270dafd1479a74498aff773a53f4d1e03fa086c32a9e8998fce5bb4a1a7875a7c7c7055705d0dde15796fb4a820'
+# HEROKU setup:
+# heroku config:set RAILS_SECRET_TOKEN="..."
+
 unless ENV['RAILS_SECRET_KEY_BASE'].blank?
   Cookitor::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']
 else
